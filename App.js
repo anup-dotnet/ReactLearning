9a6e25f325ff48c2450e8fbdbbef4a2c2b374739
Anup <anup.kumar01@outlook.com>
2024-02-23 20:39:31 +0530

    reactlearning-1

---
+++ App.js
@@ -0,0 +1,12 @@
+const parent = React.createElement("div",{id : "parent"},
+                [React.createElement("div",{id : "child1"},
+                [React.createElement("h1",{},
+                "Hello World from React"),React.createElement("h2",{},
+                "Hello World from React")]),React.createElement("div",{id : "child2"},
+                [React.createElement("h1",{},
+                "Hello World from React"),React.createElement("h2",{},
+                "Hello World from React")])]);
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+
+ root.render(parent);
